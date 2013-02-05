72b35cb4be277d595d3e523f95ec77421207dc6f
Adrien Dorsaz <adrien.dorsaz@epfl.ch>
2013-02-05 13:52:28 +0100

    Add spaces.

--- sync4ics2openmoko.sh
+++ sync4ics2openmoko.sh
@@ -12,16 +12,20 @@ shift `expr $OPTIND - 1`
 
 echo "Creating a backup copy of qtopia_db.sqlite"
 cp /home/root/Applications/Qtopia/qtopia_db.sqlite ./qtopia_db.sqlite.bak
+
 echo "Fetching files"
 for fileurl in $*
 do
 	wget --no-check-certificate --timestamping --user=$user --password=$password $fileurl
 done
+
 echo "Deleting appointments of qtopia_db"
 perl deleteqtcalappointments.pl /home/root/Applications/Qtopia/qtopia_db.sqlite
+
 echo "Deleting temporary Notes files from a previous execution"
 rm Annotator-tmp/*
 mkdir Annotator-tmp
+
 echo "Transferring events to qtopia_db"
 for filename in ./*.ics
 do
@@ -29,6 +33,7 @@ do
 done;
 echo "Removing existing Note files"
 rm -f /home/root/Applications/Annotator/0-*
+
 echo "Copying Note files"
 cp Annotator-tmp/* /home/root/Applications/Annotator/
 echo "Done"
