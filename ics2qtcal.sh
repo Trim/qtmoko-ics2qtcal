276964fc89f2e6a8e09062e870552b2e4a87f223
Adrien Dorsaz <adrien.dorsaz@epfl.ch>
2013-02-05 14:16:55 +0100

    Add spaces.

--- ics2qtcal.sh
+++ ics2qtcal.sh
@@ -9,12 +9,15 @@ done
 shift `expr $OPTIND - 1`
 
 echo "Creating temporary copy of $1 with valid lines"
+
 # Create a copy and remove X-MOZ-LASTACK lines that are not understood by Tie::iCal
 grep -v X-MOZ-LASTACK $1 >$1.tmp
 # Normalize line endings to unix format (line feed only)
 sed -e 's/\r$//g' $1.tmp >$1-2.tmp
 echo "Processing file $1"
 perl ics2qtcal.pl $verbose --ical $1-2.tmp --qtopiadb $2 --notesdirectory ./Annotator-tmp
+
+
 echo "Deleting temporary files"
 rm $1.tmp
 rm $1-2.tmp
