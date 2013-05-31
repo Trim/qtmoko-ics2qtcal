cc6fffe39f2f243cae87ab7f43a6f785bcae2e6b
Adrien Dorsaz <adrien@adorsaz.ch>
2013-05-31 13:03:33 +0200

    Revert "Add debian dir to build package"
    This reverts commit c859ab0a419b6019c1899e4ef63a80cec29dfbf3.

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(qtmoko-ics2qtcal):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="qtmoko-ics2qtcal" command="/usr/bin/qtmoko-ics2qtcal"
--- debian/qtmoko-ics2qtcal.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the qtmoko-ics2qtcal package
-#
-0 4	* * *	root	[ -x /usr/bin/qtmoko-ics2qtcal_maintenance ] && /usr/bin/qtmoko-ics2qtcal_maintenance
